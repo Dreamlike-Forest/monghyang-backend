69b1d05608a8c73b8ba169f8c65823db5679dd0d
Iwantcod <kimjoonha0426@gmail.com>
2025-07-16 16:43:05 +0900

    feat: 데이터 응답용 dto, 200 메시지 응답용 dto 분리

--- src/main/java/com/example/monghyang/domain/global/response/ResponseDto.java
+++ src/main/java/com/example/monghyang/domain/global/response/ResponseDto.java
@@ -1,24 +1,11 @@
 package com.example.monghyang.domain.global.response;
 
-import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Getter
-@Setter
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class ResponseDto<T> {
-    // 모든 정상적인 응답에 사용되는 dto
-
-    private int status = 200; // 정상적인 응답만 취급하므로, http status 200으로 고정
-    private T content; // 제네릭(T)은 객체 혹은 컬렉션 타입이 될 수 있습니다.
-
-    private ResponseDto(T content) {
-        this.content = content;
-    }
-
-    public static <T> ResponseDto<T> contentFrom(T content) {
-        return new ResponseDto<T>(content);
-    }
+@NoArgsConstructor
+public class ResponseDto {
+    // 모든 단순 응답에 사용되는 dto(추후 필드 확장 가능성 O)
+    private final int status = 200;
 }
