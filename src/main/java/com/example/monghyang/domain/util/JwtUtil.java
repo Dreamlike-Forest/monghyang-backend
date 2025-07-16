043fb0ae8cb740311c0af4ddc7ac1aeeff433cb7
Iwantcod <kimjoonha0426@gmail.com>
2025-07-16 10:14:52 +0900

    fix: jwt 파싱 예외 처리 구현

--- src/main/java/com/example/monghyang/domain/util/JwtUtil.java
+++ src/main/java/com/example/monghyang/domain/util/JwtUtil.java
@@ -1,10 +1,16 @@
 package com.example.monghyang.domain.util;
 
+import com.example.monghyang.domain.global.advice.ApplicationError;
+import com.example.monghyang.domain.global.advice.ApplicationException;
 import com.example.monghyang.domain.users.entity.Users;
 import com.example.monghyang.domain.users.repository.UsersRepository;
 import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseCookie;
 import org.springframework.stereotype.Component;
@@ -14,49 +20,59 @@ import javax.crypto.spec.SecretKeySpec;
 import java.nio.charset.StandardCharsets;
 import java.time.Duration;
 import java.util.Date;
-import java.util.Optional;
 import java.util.UUID;
 
 @Component
+@Slf4j
 public class JwtUtil {
     // jwt 토큰 생성 및 파싱을 담당하는 유틸 클래스
     private final SecretKey secretKey;
     private final Long expiration;
     private final SecretKey refreshKey;
     private final Long refreshExpiration;
-    private final UsersRepository usersRepository;
-    @Value("${app.cookie-samesite}")
-    private String cookieSamesite;
-    @Value("${app.cookie-domain}")
-    private String cookieDomain;
+    private final String cookieSamesite;
+    private final String cookieDomain;
 
     public JwtUtil(@Value("${jwt.secret}") String secret, @Value("${jwt.expiration}") Long expiration
             , @Value("${jwt.refresh-secret}") String refreshSecret, @Value("${jwt.refresh-expiration}") Long refreshExpiration
-            , UsersRepository usersRepository) {
+            , @Value("${app.cookie-samesite}") String cookieSamesite, @Value("${app.cookie-domain}") String cookieDomain) {
 
         this.secretKey = new SecretKeySpec(secret.getBytes(StandardCharsets.UTF_8), SignatureAlgorithm.HS256.getJcaName());
         this.refreshKey = new SecretKeySpec(refreshSecret.getBytes(StandardCharsets.UTF_8), SignatureAlgorithm.HS256.getJcaName());
         this.expiration = expiration;
         this.refreshExpiration = refreshExpiration;
-        this.usersRepository = usersRepository;
-
+        this.cookieSamesite = cookieSamesite;
+        this.cookieDomain = cookieDomain;
     }
 
     // 토큰을 SecretKey로 파싱하여 Claims를 얻어내는 메소드
     private Claims parseToken(String token) {
-        return Jwts.parserBuilder()
-                .setSigningKey(secretKey)
-                .build()
-                .parseClaimsJws(token)
-                .getBody();
+        try {
+            return Jwts.parserBuilder()
+                    .setSigningKey(secretKey)
+                    .build()
+                    .parseClaimsJws(token)
+                    .getBody();
+        } catch (JwtException | IllegalArgumentException e) {
+            // 토큰 파싱 예외 처리
+            log.error("토큰 파싱 에러: {}", e.getMessage());
+            throw new ApplicationException(ApplicationError.TOKEN_IMPAIRED);
+        }
     }
 
+    // refresh token 파싱 메소드
     private Claims parseRefreshToken(String token) {
-        return Jwts.parserBuilder()
-                .setSigningKey(refreshKey)
-                .build()
-                .parseClaimsJws(token)
-                .getBody();
+        try {
+            return Jwts.parserBuilder()
+                    .setSigningKey(refreshKey)
+                    .build()
+                    .parseClaimsJws(token)
+                    .getBody();
+        } catch (JwtException | IllegalArgumentException e) {
+            // 토큰 파싱 예외 처리
+            log.error("토큰 파싱 에러: {}", e.getMessage());
+            throw new ApplicationException(ApplicationError.TOKEN_IMPAIRED);
+        }
     }
 
     // 토큰에서 유저 식별자 추출
@@ -101,6 +117,12 @@ public class JwtUtil {
         return claims.getExpiration().before(new Date());
     }
 
+    // refresh token 만료여부 검증
+    public boolean isExpiredRefreshToken(String token) {
+        Claims claims = parseRefreshToken(token);
+        return claims.getExpiration().before(new Date());
+    }
+
     // access token 발급
     public ResponseCookie createAccessToken(Long userId, String role) {
         String access = Jwts.builder()
@@ -134,14 +156,37 @@ public class JwtUtil {
                 .maxAge(refreshExpiration/1000).build();
     }
 
-    // 특정 유저가 서버로 전송한 refresh token값이 해당 유저의 테이블의 refresh token 값과 일치하는지 검증
-    public boolean isExistRefreshToken(Long userId, String refreshToken) {
-        Optional<Users> user = usersRepository.findById(userId);
-        if(user.isEmpty()) {
-            return false;
+    // 특정 유저가 서버로 전송한 refresh token의 tid값이 해당 유저의 테이블의 refresh token tid 값과 일치하는지 검증
+    // 만료 여부 검증
+    public boolean verifyRefreshToken(HttpServletRequest request, String prevRefreshTokenId) {
+        String refreshToken = null;
+
+        // 요청에 쿠키가 존재하는지 확인
+        if(request.getCookies() != null) {
+            // 쿠키에서 refresh token을 찾는 과정
+            for(Cookie cookie : request.getCookies()) {
+                if(cookie.getName().equals("refresh_token")) {
+                    refreshToken = cookie.getValue();
+                    break;
+                }
+            }
+        }
+        if(refreshToken == null) {
+            // refresh token 존재하지 않을 시 예외 발생
+            throw new ApplicationException(ApplicationError.AUTH_INFO_NOT_FOUND);
         }
 
-        return user.get().getRefreshToken().equals(refreshToken);
+        if(isExpiredRefreshToken(refreshToken)) {
+            // refresh token 만료 시 예외 발생
+            throw new ApplicationException(ApplicationError.TOKEN_EXPIRED);
+        }
+        // 기존에 저장된 refresh token tid와 토큰 갱신 요청에 담긴 refresh token의 tid가 동일한지 검증
+        if(!getRefreshId(refreshToken).equals(prevRefreshTokenId)) {
+            // 다르다면 동시접속 발생한 것으로 간주
+            throw new ApplicationException(ApplicationError.CONCURRENT_CONNECTION);
+        }
+
+        return true;
     }
 
     public ResponseCookie createMax0AccessToken() { // 수명이 0인 access token 발급: 로그아웃 용
