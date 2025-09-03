75adb43dc7848c53737e053731194504ba05663d
Iwantcod <kimjoonha0426@gmail.com>
2025-09-03 14:09:10 +0900

    fix: 로그인 시 사용되는 쿼리문에 email을 like로 조회하는 쿼리문 동등연산으로 변경

--- src/main/java/com/example/monghyang/domain/users/repository/UsersRepository.java
+++ src/main/java/com/example/monghyang/domain/users/repository/UsersRepository.java
@@ -13,7 +13,7 @@ public interface UsersRepository extends JpaRepository<Users, Long> {
     @Query("select u from Users u join fetch u.role where u.email like %:email%")
     List<Users> findByEmailJoinedRole(@Param("email") String email);
 
-    @Query("select u from Users u join fetch u.role where u.email like %:email% and u.isDeleted = false")
+    @Query("select u from Users u join fetch u.role where u.email = :email and u.isDeleted = false")
     Optional<Users> findByEmailActiveJoinedRole(@Param("email") String email);
     Optional<Users> findByoAuth2Id(String oAuth2Id);
     boolean existsByEmail(String email);
