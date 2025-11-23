74173065473e7a41dcbfdba92f14a1d303e6c813
김준하 <kimjoonha0426@gmail.com>
2025-11-23 19:01:26 +0900

    feat: 식별자로 유저 조회, 자기 자신의 정보 조회 API 추가. 식별자/이메일로 유저 조회 시 민감정보 제거

--- src/main/java/com/example/monghyang/domain/users/dto/ResUsersDto.java
+++ src/main/java/com/example/monghyang/domain/users/dto/ResUsersDto.java
@@ -14,11 +14,7 @@ public class ResUsersDto {
     private final String users_email;
     private final String users_nickname;
     private final String users_name;
-    private final String users_phone;
-    private final LocalDate users_birth;
     private final String users_gender;
-    private final String users_address;
-    private final String users_address_detail;
 
     private ResUsersDto(Users users) {
         this.users_id = users.getId();
@@ -26,11 +22,7 @@ public class ResUsersDto {
         this.users_email = users.getEmail();
         this.users_nickname = users.getNickname();
         this.users_name = users.getName();
-        this.users_phone = users.getPhone();
-        this.users_birth = users.getBirth();
         this.users_gender = (users.getGender() == Boolean.FALSE) ? "man" : "woman";
-        this.users_address = users.getAddress();
-        this.users_address_detail = users.getAddressDetail();
     }
 
     public static ResUsersDto usersJoinedWithRoleToDto(Users users) {
