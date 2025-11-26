f2e72163dcb075986063ae325018081b928507ee
김준하 <kimjoonha0426@gmail.com>
2025-11-26 20:38:01 +0900

    feat: LoggingFilter 1차 구현

--- src/main/java/com/example/monghyang/domain/logging/LoggingFilter.java
+++ src/main/java/com/example/monghyang/domain/logging/LoggingFilter.java
@@ -22,16 +22,28 @@ public class LoggingFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         long startTime = System.currentTimeMillis();
-
+        String traceId = UUID.randomUUID().toString();
         MDC.put("requestURI", request.getRequestURI());
         MDC.put("method", request.getMethod());
-        MDC.put("traceId", UUID.randomUUID().toString());
+        MDC.put("traceId", traceId);
         MDC.put("clientIp", request.getRemoteAddr());
 
         try {
             filterChain.doFilter(request, response);
         } finally {
             long duration = System.currentTimeMillis() - startTime;
+            // 아래 두 개의 값은 인증 정보가 없을 시 null / Anonymous 로 설정된다.
+            String userId = MDC.get("userId");
+            String userRole = MDC.get("userRole");
+
+            log.info("REQ_END traceId={} method={} path={} status={} durationMs={} userId={} roles={}",
+                    traceId,
+                    request.getMethod(),
+                    request.getRequestURI(),
+                    response.getStatus(),
+                    duration,
+                    userId,
+                    userRole);
             MDC.clear();
         }
     }
