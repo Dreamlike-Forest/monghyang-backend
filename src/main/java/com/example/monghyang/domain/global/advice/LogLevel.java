bdb0f81e5d23fbaac10429cbd51a05796dc4ccf7
김준하 <kimjoonha0426@gmail.com>
2025-11-26 17:13:55 +0900

    feat: ApplicationError에 에러 종류, 로그 레벨, stacktrace 출력 유무 정보에 대한 필드 추가. ErrorType, LogLevel enum 정의.

---
+++ src/main/java/com/example/monghyang/domain/global/advice/LogLevel.java
@@ -0,0 +1,11 @@
+package com.example.monghyang.domain.global.advice;
+
+/**
+ * 중요도 정도
+ */
+public enum LogLevel {
+    INFO, // 정상적인 동작을 기록
+    DEBUG, // 정상적인 동작 상세히 기록
+    WARN, // 주의: 주요 비즈니스 로직 수행과 관련
+    ERROR // 심각: 반드시 로그를 별도의 파일로 기록
+}
