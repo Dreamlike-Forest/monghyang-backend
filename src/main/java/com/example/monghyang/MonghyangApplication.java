347421ff75d9c8017d0ef24ce65b14baf79efb22
김준하 <kimjoonha0426@gmail.com>
2025-12-02 20:45:54 +0900

    feat: JVM timezone asia/seoul로 고정

--- src/main/java/com/example/monghyang/MonghyangApplication.java
+++ src/main/java/com/example/monghyang/MonghyangApplication.java
@@ -4,11 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 @EnableWebSecurity(debug=true)
 public class MonghyangApplication {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
 		SpringApplication.run(MonghyangApplication.class, args);
 	}
 
